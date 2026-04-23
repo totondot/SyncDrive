3f0e287e2db01207bd240a9ce850d17a38ac4dc5
Abrar Zahin <abrar.zahin.office@gmail.com>
2026-04-24 03:02:53 +0600

    Delete SyncDrive directory

--- SyncDrive/app/src/main/java/com/example/syncdrive/VehicleStatus.kt
+++
@@ -1,7 +0,0 @@
-package com.example.syncdrive
-
-// Model: Holds the data for the vehicle's battery and range
-data class VehicleStatus(
-    var batteryPercentage: Int, // e.g., 85 for 85%
-    var estimatedRangeKm: Double // e.g., 120.5 for kilometers left
-)
